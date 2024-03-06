a662ff083da04c576e794ca2d2ff38b44a0737d8
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2024-03-05 19:45:43 -0600

    Update main.cpp

--- CS110/Exercise/main.cpp
+++ CS110/Exercise/main.cpp
@@ -5,19 +5,7 @@ using namespace std;
 
 int main()
 {
-  /*************************************************
-   * Student Name: John Palaganas
-   * Student Number: 200480405
-   * Replit User Name: <~~jpl653~~<<
-   * Assignment Number: CS 110 Assignment 1
-   * Program Name: A1Q3.cpp
-   * Last Modified:
-   *************************************************/
-
-    // follow up question: on number 4. Do we need to create a data types for my friend? Or we can just
-   // mention their name by implemeting cout ...
-
-    // Extended Welcome Phase
+  
     cout << "Hello my name is John Palaganas. \n" << endl;
 
     // Greet friend with a message
